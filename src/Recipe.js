e6ce8f917da8bbf586b96661f90997d167a9a85f
Vignesh <vigneshsekhar21@gmail.com>
2023-01-21 16:02:08 +0530

    styling

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,9 +1,19 @@
+import "./Recipe.css"
+
 export default function Recipe(props) {
+    const cal = (props.calories).toFixed(2)
     return(
-        <div>
-            <h1>{props.title}</h1>
-            <p>{props.calories}</p>
-            <img src={props.image} alt=""/>
+        <div className="recipe">
+            <h3>{props.title}</h3>
+            <div className="image"><img src={props.image} alt=""/></div>
+            <div className="ingredient">
+            <ol>
+                {props.ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            </div>
+            <p>Calories: {cal}</p>
         </div>
     );
 }
